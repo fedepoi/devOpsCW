84d76e4a088214a3e3315deb41cd61d833f69349
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-11 14:35:11 +0000

    added new copy of the file

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
-class Dec2Hex{  
-   public static int Arg1;
-     public static void main(String args[])    {
-        if (args.length > 0)         {    
-        try             {  
-              Arg1 = Integer.parseInt(args[0]);      
-      }           
-  catch (NumberFormatException e)             
- {System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);             }   
-     }
-char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-int rem, num; 
-num = Arg1; 
-String hexadecimal="";
-          System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
-        while(num != 0)         {   
- rem=num%16;     hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;         }   
- System.out.println("Hexadecimal representation is : " + hexadecimal);   
- System.out.println("another change");
+class Dec2Hex{     
+public static int Arg1;     
+public static void main(String args[])    
+{
+        if (args.length > 0)         
+{            try   
+          {                
+Arg1 = Integer.parseInt(args[0]);            
+}             
+catch (NumberFormatException e)              
+{System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(1);             
+}        
 }
-} 
+char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+int rem, num;
+ num = Arg1; 
+String hexadecimal="";
+          System.out.println("Converting the Decimal Value " + num + " to Hex...");
+         while(num != 0)         {
+    rem=num%16;     
+hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;         
+}    
+System.out.println("Hexadecimal representation is : " + hexadecimal);
+    }
+}
