649cc949ff870c05801c4d2df73eac11026bb18c
fedepoi <federico.vivaldo@gmail.com>
2020-11-11 19:37:42 +0000

    added scanner functionality

--- Dec2Hex.java/src/Dec2Hex.java
+++ Dec2Hex.java/src/Dec2Hex.java
@@ -10,15 +10,26 @@ class Dec2Hex{
  {System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);             }   
      }
+        
+        
 char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 int rem, num; 
-num = Arg1; 
+System.out.println("please enter a valide number");
+//creating scanner class to read console input
+Scanner consoleReader = new Scanner(System.in);
+if(consoleReader.hasNextInt()){
+ num=consoleReader.nextInt();
+   if(num<0){System.out.println("please enter a positive integer");}
+} else 
+
+
+num = consoleReader.nextInt(); 
 String hexadecimal="";
           System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
         while(num != 0)         {   
  rem=num%16;     hexadecimal= ch[rem] + hexadecimal;
             num= num/16;         }   
  System.out.println("Hexadecimal representation is : " + hexadecimal);   
- System.out.println("\n try to access from windows"); 
+  
 }
 } 
