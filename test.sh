bc603a83bf8debccb257b7d2019d806fef305ed5
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-18 15:31:11 +0000

    modified test script added negative number check

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 javac Dec2Hex.java/src/Dec2Hex.java
 cd Dec2Hex.java/src
-echo test with no argument:
+echo test with no argument: 
 java Dec2Hex
+echo ""
 echo test with integer argument supplied:
 java Dec2Hex 15683
+echo ""
 echo test with String provided:
 java Dec2Hex 4hundred
+echo ""
+echo test with a negative number:
+java Dec2Hex -895
+echo ""
