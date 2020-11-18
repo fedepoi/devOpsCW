53c7c5239198d4c970b1957227fb67f4c84ea844
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-18 10:20:54 +0000

    added test script

---
+++ test.sh
@@ -0,0 +1,8 @@
+javac Dec2Hex.java/src/Dec2Hex.java
+cd Dec2Hex.java/src
+echo test with no argument:
+java Dec2Hex
+echo test with integer argument supplied:
+java Dec2Hex 15683
+echo test with String provided:
+java Dec2Hex 4hundred
