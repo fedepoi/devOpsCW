6caf13f952bed4e5ab26b48dcb72a93f3e0d9bba
fedepoi <federico.vivaldo@gmail.com>
2020-11-18 16:13:55 +0100

    deleted test class

--- Dec2Hex.java/src/Dec2Hex.java
+++ Dec2Hex.java/src/Dec2Hex.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 class Dec2Hex{     
     public static int Arg1;     
@@ -8,15 +9,14 @@ class Dec2Hex{
        System.out.println("please provide a positive integer");
        System.exit(0);
        }*/
-       
-       
        if(args.length==0){
            System.out.println("please provide a number");
            System.exit(0);
        } 
        else{
-        if (args.length > 0)         {            
-            try             {                
+        if (args.length > 0) {            
+            try{
+                
                 Arg1 = Integer.parseInt(args[0]);  
             }             
             catch (NumberFormatException e)              
@@ -25,7 +25,7 @@ class Dec2Hex{
             }        
         } 
        }
-            testNeg(Arg1);
+        
         
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num; 
@@ -41,18 +41,6 @@ class Dec2Hex{
         System.out.println("Hexadecimal representation is : " + hexadecimal); 
     }
     
-    public static void testNeg(int i){
-    try{
-        if(i<0){
-        System.out.println("the provided value is negative, the app only works with positive integers");}
-        } 
-    
-    catch (NumberFormatException e)              
-            {              
-            System.exit(0);             
-            }       
-    
-    }
     
     
     
