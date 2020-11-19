0cc786a75f3ea9d28cb39212d92d95fb79e5e07a
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-19 09:15:56 +0000

    script test modified

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
+echo ----------Start testing script----------
+
 javac Dec2Hex.java/src/Dec2Hex.java
-cd Dec2Hex.java/src
-echo test with no argument: 
-java Dec2Hex
+#cd Dec2Hex.java/src
+echo ----------test with no argument----------
+java Dec2Hex.java/src/Dec2Hex.java
 echo ""
-echo test with integer argument supplied:
-java Dec2Hex 15683
+echo ----------test with integer argument supplied----------
+java Dec2Hex.java/src/Dec2Hex.java 15683
 echo ""
-echo test with String provided:
-java Dec2Hex 4hundred
+echo ----------test with String argument----------
+java Dec2Hex.java/src/Dec2Hex.java 4hundred
 echo ""
-echo test with a negative number:
-java Dec2Hex -895
+echo ----------test with a negative number----------
+java Dec2Hex.java/src/Dec2Hex.java -895
 echo ""
+
+
