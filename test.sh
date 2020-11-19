93a56b120d866738dce4ca9eb0bea7b2de5bd3e5
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-19 10:19:55 +0000

    added test more than one value in test script

--- test.sh
+++ test.sh
@@ -14,5 +14,10 @@ echo ""
 echo ----------test with a negative number----------
 java Dec2Hex -895
 echo ""
-
+echo ----------test with a number bigger than the supported one----------
+java Dec2Hex 2147483649
+echo ""
+echo ----------test with more than one value entered----------
+java Dec2Hex 85 78999 four 89
+echo ""
 
