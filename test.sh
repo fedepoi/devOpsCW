263c1194d5266b4b8c2e4204297ba4b05f19b044
Ubuntu <ubuntu@ip-172-31-73-24.ec2.internal>
2020-11-21 23:17:02 +0000

    added script comments

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
+#start of the test script
 echo ----------Start testing script----------
-
+#compiling java file
 javac Dec2Hex.java/src/Dec2Hex.java
+#move into java file directory
 cd Dec2Hex.java/src
 echo ""
 
 echo ----------handle program fail----------
 echo ""
+#this test will run the program with no argument
 echo ----------test with no argument----------
 java Dec2Hex
 STATUS="${?}"
@@ -17,7 +20,7 @@ else
         echo -e "the program failed"
 fi
 echo ""
-
+#this test will run the program giving an integer as input
 echo ----------test with integer argument supplied----------
 java Dec2Hex 15683
 STATUS="${?}"
@@ -30,7 +33,7 @@ else
 
 fi
 echo ""
-
+#this test will run the program with a negative integer as input
 echo ----------test with a negative number----------
 java Dec2Hex -895  || STATUS=1
 
@@ -43,7 +46,7 @@ else
 
 fi 
 echo ""
-
+#this test will run the program with a number bigger than integer
 echo ----------test with a number bigger than the supported one----------
 java Dec2Hex 2147483649
 STATUS="${?}"
@@ -55,7 +58,7 @@ else
         echo -e  "the program failed"
 fi
 echo ""
-
+#this test will run the program with more than one argument supplied
 echo ----------test with more than one value entered----------
 java Dec2Hex 85 78999 four 89 || STATUS=1
 if [ "${STATUS}" -eq 0 ]
@@ -66,7 +69,7 @@ else
         echo -e  "the program failed"
 fi
 echo ""
-
+#this test will run the program with a string value input
 echo ----------test with String argument----------
 java Dec2Hex 4hundred || STATUS=1
 if [ "${STATUS}" -eq 0 ]
